c18f867db6c5976cdb4f9c37325a82723b6b270b
Rafael-Bardisa <rafael.bardisa01@estudiant.upf.edu>
2022-02-26 20:23:13 +0100

    bigram incompleto

--- lab2/src/main/java/upf/edu/BiGram.java
+++ lab2/src/main/java/upf/edu/BiGram.java
@@ -1,5 +1,8 @@
 package upf.edu;
 
+import java.util.List;
+import java.util.Objects;
+
 public class BiGram {
     private String first;
     private String last;
@@ -7,5 +10,29 @@ public class BiGram {
     public BiGram(String first, String last){
         this.first = first;
         this.last = last;
+    }
+
+    public List<BiGram> fromExtendedTweet(ExtendedSimplifiedTweet tweet){
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, last);
+    }
+
+
+    @Override
+    public boolean equals(Object other){
+        boolean result;
+        if((other == null) || (getClass() != other.getClass())){
+            return (false);
+        }
+        BiGram otherBiGram = (BiGram) other;
+        return (this.first.equals(otherBiGram.first) &&  this.last.equals(otherBiGram.last));
+    } // end equals
+
+    private static String normalise(String word) {
+        return word.trim().toLowerCase();
     }
 }
